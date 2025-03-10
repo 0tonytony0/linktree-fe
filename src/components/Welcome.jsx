0d4b37b662a3f3b0e15829c21e1d180cd3129189
vibhu <vpvibhu2002@gmail.com>
2025-03-11 02:39:12 +0530

    api error update

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
-import '../pages/WelcomePage'
+import "../pages/WelcomePage";
 import { Link } from "react-router-dom";
 
-const Welcome = ({updateHandler}) => {
-  
-
+const Welcome = ({ updateHandler }) => {
   return (
     <div className="welcome-content">
-          <img src="images/spark-logo.svg" alt="Spark Logo" className="logo" />
-          <div className="welcome-content-main">
-            <h1>Sign up to your Spark</h1>
-            <h3>Welcome to Spark</h3>
-            {/* Clicking this button will show the CreateAccount component */}
-            <button className="email-btn" onClick={()=>updateHandler()}>
-              Continue with email
-            </button>
-            <p>
-              Already have an account? <Link to="/login">Sign in</Link>
-            </p>
-          </div>
-        </div>
+      <img src="images/spark-logo.svg" alt="Spark Logo" className="logo" />
+      <div className="welcome-content-main">
+        <h1>Sign up to your Spark</h1>
+        <h3>Welcome to Spark</h3>
+        {/* Clicking this button will show the CreateAccount component */}
+        <button className="email-btn" onClick={() => updateHandler()}>
+          Continue with email
+        </button>
+        <p>
+          Already have an account? <Link to="/login">Sign in</Link>
+        </p>
+      </div>
+    </div>
   );
 };
 
